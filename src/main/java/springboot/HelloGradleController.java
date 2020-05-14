3d9a97375606437364c6ec5c6b152d22f901df69
Sanu Kumar <sanu.kumar310@gmail.com>
2020-05-14 19:23:08 -0400

    first push

---
+++ src/main/java/springboot/HelloGradleController.java
@@ -0,0 +1,14 @@
+package springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+
+    @GetMapping
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
+
+}
